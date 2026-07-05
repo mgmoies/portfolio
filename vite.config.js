33cb9bc85bac356d280dc90924d1a7f26e673ead
mgmoies <tapyothin.v@kkumail.com>
2026-07-05 23:22:16 +0700

    Configure for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '', // Empty string ensures relative paths for assets, ideal for GitHub Pages
 })
